0b70fae623222d6e9bb8cd1259e7724687d4fca5
LeedorReichman <leedor.yacobi@post.runi.ac.il>
2026-01-02 23:11:16 +0200

    fixed

--- Binomial.java
+++ Binomial.java
@@ -1,45 +1,52 @@
 /** Computes the binomial(n,k) function. */
 public class Binomial {	
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 		if (args.length < 2) {
 			System.out.println("Usage: java Binomial n k");
 			return;
 		}
 		int n = Integer.parseInt(args[0]);
 		int k = Integer.parseInt(args[1]);
+
+		if (n <= 25) {
+			System.out.println("binomial1(" + n + "," + k + ") = " + binomial1(n, k));
+		} else {
+			System.out.println("binomial1 skipped for n > 25 (too slow)");
+		}
+		System.out.println("binomial(" + n + "," + k + ") = " + binomial(n, k));
 	}
 
 	// Computes the Binomial function, basic version.
-	public static int binomial1(int n, int k) { 
-		if (k > n) return 0;
-		if (k == 0 || n == 0) return 1;
+	public static long binomial1(int n, int k) {
+		if (k > n) return 0L;
+		if (k == 0 || n == 0) return 1L;
 		return binomial1(n - 1, k) + binomial1(n - 1, k - 1);
 	 }
 	
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
-		int[][] memo = new int[n + 1][k + 1];
+	public static long binomial(int n, int k) {
+		long[][] memo = new long[n + 1][k + 1];
 		for (int i = 0; i <= n; i++) {
 			for (int j = 0; j <= k; j++) {
-				memo[i][j] = -1;
+				memo[i][j] = -1L;
 			}
 		}
 		return binomial(n, k, memo);
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
-		if (memo[n][k] != -1) {
+	private static long binomial(int n, int k, long[][] memo) {
+		if (memo[n][k] != -1L) {
 			return memo[n][k];
 		}
 		// Base case
-		if ((k > n)) {
-		   	memo[n][k] = 0; 
-		   	return 0;
+		if (k > n) {
+			memo[n][k] = 0L;
+			return 0L;
 		}
 		// Another base case
 		if (n == 0 || k == 0) {
-		   	memo[n][k] = 1; 
-		   	return 1;
+			memo[n][k] = 1L;
+			return 1L;
 		}
 		memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
 		return memo[n][k];
